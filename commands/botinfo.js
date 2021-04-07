bd4df8f15abaf5c2e9739b895fd4c1e16d6a4d7b
TheWaildug <64437684+TheWaildug@users.noreply.github.com>
2021-04-07 16:54:12 -0500

    updated so you can view what platform the bot is hosted on.
    Linux: The hoster.
    Windows: Ran on Visual Studio Code on computer. Usually only used when testing features for bot.

--- commands/botinfo.js
+++ commands/botinfo.js
@@ -25,12 +25,17 @@ module.exports = {
           embedcolor.save()
         }
         console.log(`bot info`)
+        let platform = process.platform;
+    if(platform == "linux"){
+      platform = "Linux"
+    }else if(platform == "win32"){
+      platform = "Windows (Running on Visual Studio Code)"
+    }
     const emoji = client.emojis.cache.get("810267394999058432")
     const owner = await client.users.fetch("432345618028036097")
     const embed = new Discord.MessageEmbed()
-    
     .setAuthor(`Information about ${client.user.username}.`,client.user.displayAvatarURL())
-    .setDescription(`**Bot** - ${client.user.tag} - ${client.user.id}\n**Created with** ${emoji}\n**Sir Countalot Version** - ${jsonfile.version}\n**Node JS Version** - ${process.version}\n**Time since last start** - ${ms(client.uptime,{long: true})}`)
+    .setDescription(`**Bot** - ${client.user.tag} - ${client.user.id}\n**Created with** ${emoji}\n**Operating System** -  ${platform}\n **Sir Countalot Version** - ${jsonfile.version}\n**Node JS Version** - ${process.version}\n**Time since last start** - ${ms(client.uptime,{long: true})}`)
 .setColor(embedcolor.color)
 message.channel.send(embed)
 return;
